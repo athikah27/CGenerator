7307cd053940c1343b1cc8815569465ddbfa831a
Leviathan1108 <lutfifuadradityawan@gmail.com>
2025-03-19 07:28:55 +0700

    update

--- resources/views/layout/v_layout.blade.php
+++ resources/views/layout/v_layout.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Certificate Generator</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
   </head>
   <body>
 
     @include ('layout.nav')
     @yield ('content')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.js') }}"></script>
   </body>
 </html>
