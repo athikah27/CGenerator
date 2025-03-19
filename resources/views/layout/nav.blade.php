147f7d707d5ff6efcb962b660d930173bca15b5d
Leviathan1108 <lutfifuadradityawan@gmail.com>
2025-03-19 08:02:05 +0700

    navbar

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,7 +1,17 @@
-<nav class="nav flex-column">
-  <a class="nav-link active" aria-current="page" href="/">Home</a>
-  <a class="nav-link" href="/certificate">Certificates</a>
-  <a class="nav-link" href="/templates">Templates</a>
-  <a class="nav-link" href="/settings">Settings</a>
-  <a class="nav-link disabled" aria-disabled="true">disabled</a>
-</nav>
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="/">Certificate Generator</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/certificate">Certificates</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/templates">Templates</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/settings">Settings</a>
+  </li>
+</ul>
